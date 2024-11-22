84e492f74176cd213972c114a5c6e1d5a658be85
Oranos <69981593+oranos007@users.noreply.github.com>
2024-11-22 16:05:20 +0100

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+   <footer>
+    <p>Created By <a href="https://ankitjha.vercel.app" target='_blank'>Ankit Jha</a></p>
+   </footer>
+  )
+}
+
+export default Footer
